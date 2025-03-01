37ca25f6b79df613bbb0edc6296d72f747b5468a
MethmiNPCS <npcsmethmi@gmail.com>
2025-03-01 09:36:27 +0530

    Added write file feature successfully

--- app.js
+++ app.js
@@ -1,7 +1,15 @@
 console.log("Hello, this is my first Node.js app!");
 
+// Read file
 const fs = require('fs');
 fs.readFile('file.txt', 'utf8', function (err, data) {
     if (err) throw err;
     console.log(data);
+});
+
+// Write file
+const fs1 = require('fs'); 
+fs1.writeFile('file1.txt', 'Hello World!', function (err) { 
+  if (err) throw err; 
+  console.log('File saved!'); 
 });
