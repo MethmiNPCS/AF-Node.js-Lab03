d56ab3ecf0973d7ff4e1930e0dec040b09eaa2cc
MethmiNPCS <npcsmethmi@gmail.com>
2025-03-01 09:23:47 +0530

    Added file.txt successfully

--- app.js
+++ app.js
@@ -1,1 +1,7 @@
 console.log("Hello, this is my first Node.js app!");
+
+const fs = require('fs');
+fs.readFile('file.txt', 'utf8', function (err, data) {
+    if (err) throw err;
+    console.log(data);
+});
