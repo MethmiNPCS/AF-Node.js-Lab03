19bead70e85449ea971eea493cdb8f16ded8bb1d
MethmiNPCS <npcsmethmi@gmail.com>
2025-03-01 09:40:55 +0530

    Added web server funtionality

--- app.js
+++ app.js
@@ -13,3 +13,10 @@ fs1.writeFile('file1.txt', 'Hello World!', function (err) {
   if (err) throw err; 
   console.log('File saved!'); 
 });
+
+const http = require('http'); 
+http.createServer(function (req, res) { 
+  res.writeHead(200, {'Content-Type': 'text/html'}); 
+  res.write('Hello World!'); 
+  res.end(); 
+}).listen(8080);
